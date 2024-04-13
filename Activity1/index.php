57b8abd444c012d6f21e1bb4141956b88f97e404
ChrisCayabyab <142383617+ChrisCayabyab@users.noreply.github.com>
2024-04-13 22:20:55 +0800

    Add files via upload

---
+++ Activity1/index.php
@@ -0,0 +1,241 @@
+<html>
+   <head>
+         <title>Activity 1</title>	
+         <!-- Latest compiled and minified CSS -->
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<script
+  src="https://code.jquery.com/jquery-3.7.1.js"
+  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+  crossorigin="anonymous"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  
+		
+   </head>
+   <body>
+      <h1>Activity 1</h1>
+      <div class="container-fluid">
+      <table class="table">
+         <tr class="danger">
+            <th>ID</th>
+            <th>Name</th>
+            <th>Remarks</th>
+         </tr>
+         <tr>
+            <td>001</td>
+            <td>Alex</td>
+            <td>Passed</td>
+         </tr>
+         <tr>
+            <td>002</td>
+            <td>Cris</td>
+            <td>Passed</td>
+         </tr>
+         <tr>
+            <td>00</td>
+            <td>Jack</td>
+            <td>Failed</td>
+         </tr>
+      </table>
+      <table class="table table-striped">
+         <tr class="info">
+            <th>ID</th>
+            <th>Name</th>
+            <th>Remarks</th>
+         </tr>
+         <tr>
+            <td>001</td>
+            <td>Alex</td>
+            <td>Passed</td>
+         </tr>
+         <tr>
+            <td>002</td>
+            <td>Cris</td>
+            <td>Passed</td>
+         </tr>
+         <tr>
+            <td>00</td>
+            <td>Jack</td>
+            <td class="danger">Failed</td>
+         </tr>
+      </table>
+      <table class="table table-condensed">
+         <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Remarks</th>
+         </tr>
+         <tr>
+            <td>001</td>
+            <td>Alex</td>
+            <td>Passed</td>
+         </tr>
+         <tr>
+            <td>002</td>
+            <td>Cris</td>
+            <td>Passed</td>
+         </tr>
+         <tr>
+            <td>00</td>
+            <td>Jack</td>
+            <td>Failed</td>
+         </tr>
+      </table>
+      <table class="table table-hover table-bordered">
+         <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Remarks</th>
+         </tr>
+         <tr>
+            <td>001</td>
+            <td>Alex</td>
+            <td>Passed</td>
+         </tr>
+         <tr>
+            <td>002</td>
+            <td>Cris</td>
+            <td>Passed</td>
+         </tr>
+         <tr>
+            <td>00</td>
+            <td>Jack</td>
+            <td>Failed</td>
+         </tr>
+      </table>
+      <table class="table table-bordered">
+         <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Remarks</th>
+         </tr>
+         <tr>
+            <td>001</td>
+            <td>Alex</td>
+            <td>Passed</td>
+         </tr>
+         <tr>
+            <td>002</td>
+            <td>Cris</td>
+            <td>Passed</td>
+         </tr>
+         <tr>
+            <td>00</td>
+            <td>Jack</td>
+            <td>Failed</td>
+         </tr>
+      </table>
+      </div>
+      <hr />
+      <br />
+
+      <div class="container">
+         <p class="text-success bg-success">Sucess</p>
+         <p class="text-danger bg-danger">Danger</p>
+         <p class="text-info bg-info">Info</p>
+         <p class="text-info bg-info">Primary</p>
+         <p class="text-muted">Muted</p>
+         <p class="text-warning bg-warning">Warning</p>
+
+         <p class="lead">Information Technology</p>
+         <p class="small">Information Technology</p>
+         <p class="text-center">Information Technology</p>
+         <p class="text-right">Information Technology</p>
+         <p class="text-uppercase">Information Technology</p>
+         <p class="text-lowercase">Information Technology</p>
+      </div>
+      <hr />
+      <br />
+
+      <div class="container">
+         <p class="text-success bg-success">Sucess</p>
+         <p class="text-danger bg-danger">Danger</p>
+         <p class="text-info bg-info">Info</p>
+         <p class="text-info bg-info">Primary</p>
+         <p class="text-muted">Muted</p>
+         <p class="text-warning bg-warning">Warning</p>
+
+         <p class="lead">Information Technology</p>
+         <p class="small">Information Technology</p>
+         <p class="text-center">Information Technology</p>
+         <p class="text-right">Information Technology</p>
+         <p class="text-uppercase">Information Technology</p>
+         <p class="text-lowercase">Information Technology</p>
+      </div>
+      <hr />
+      <br />
+
+      <div class="container">
+         <p class="text-success bg-success">Sucess</p>
+         <p class="text-danger bg-danger">Danger</p>
+         <p class="text-info bg-info">Info</p>
+         <p class="text-info bg-info">Primary</p>
+         <p class="text-muted">Muted</p>
+         <p class="text-warning bg-warning">Warning</p>
+
+         <p class="lead">Information Technology</p>
+         <p class="small">Information Technology</p>
+         <p class="text-center">Information Technology</p>
+         <p class="text-right">Information Technology</p>
+         <p class="text-uppercase">Information Technology</p>
+         <p class="text-lowercase">Information Technology</p>
+      </div>
+      <hr />
+      <br />
+
+      <div class="container">
+         <p class="text-success bg-success">Sucess</p>
+         <p class="text-danger bg-danger">Danger</p>
+         <p class="text-info bg-info">Info</p>
+         <p class="text-info bg-info">Primary</p>
+         <p class="text-muted">Muted</p>
+         <p class="text-warning bg-warning">Warning</p>
+
+         <p class="lead">Information Technology</p>
+         <p class="small">Information Technology</p>
+         <p class="text-center">Information Technology</p>
+         <p class="text-right">Information Technology</p>
+         <p class="text-uppercase">Information Technology</p>
+         <p class="text-lowercase">Information Technology</p>
+      </div>
+
+      <hr />
+      <br />
+
+      <div class="container-fluid">
+         <div class="row">
+            <div class="col-md-1" style="background: green;">grid</div>
+            <div class="col-md-1" style="background: red;">grid</div>
+            <div class="col-md-5" style="background: blue;">grid</div>
+         </div>
+         
+         <div class="row">
+            <div class="col-md-6" style="background:orange;">grid</div>
+         </div>
+         
+      </div>
+      <div class="container-fluid">
+         <header class="jumbotron">
+            <h1>Container</h1>
+         </header>
+      </div>
+      <hr />
+      <br />
+      <div class="container">
+         <header class="jumbotron">
+            <h1>Container</h1>
+         </header>
+      </div>
+      <hr />
+      <br />
+      <div class="jumbotron">
+         <h1>Hewo</h1>
+      </div>
+  
+   </body>
+</html>
